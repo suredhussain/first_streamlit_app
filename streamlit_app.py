f228a9e17395411d59165e91361e66cca0577a6d
suredhussain <32246047+suredhussain@users.noreply.github.com>
2022-11-13 20:32:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,10 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_row=my_cur.fetchone()
+streamlit.text("Fruit load list contains:")
+streamlit.text(my_data_row)
+
